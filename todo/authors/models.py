1d61860003311317a6f16297cbb2cf5f4b35be6b
Artem026 <kris_my@mail.ru>
2022-02-02 20:46:33 +0300

    les_2

--- todo/authors/models.py
+++ todo/authors/models.py
@@ -6,4 +6,20 @@ class Authors(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
-    
+
+    def __str__(self):
+        return self.first_name
+
+class Biography(models.Model):
+    text = models.TextField()
+    author = models.OneToOneField(Authors, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.author.first_name} {self.author.last_name}'
+
+class Book(models.Model):
+    name = models.CharField(max_length=64)
+    author = models.ManyToManyField(Authors)
+
+    def __str__(self):
+        return self.name
--- todo/authors/views.py
+++ todo/authors/views.py
@@ -1,7 +1,9 @@
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
 from .models import Authors
 from .serializers import AuthorModelSerializer
 
 class AuthorModelViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = Authors.objects.all()
     serializer_class = AuthorModelSerializer
